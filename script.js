e5c3500bff0e4203b28fe5b5620b6efcf4a64901
vishal Pawar <133097620+vishalpawar132@users.noreply.github.com>
2023-06-20 17:14:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+window.onload = function() {
+    var element = document.getElementById("level");
+    var level = 0;
+    
+    while (element.parentNode) {
+        element = element.parentNode;
+        level++;
+    }
+    
+    alert("The level of the element is: " + level);
+};
